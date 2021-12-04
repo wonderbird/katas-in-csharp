6f9765608cd1c58984dddfc76eca0f7d5b47e9c1
Stefan Boos <sboos@igus.net>
2021-12-04 18:11:44 +0100

    feat: remove comment-only strings

--- StripComments/StripComments.Logic.Tests/StripCommentsSolutionTest.cs
+++ StripComments/StripComments.Logic.Tests/StripCommentsSolutionTest.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Diagnostics.CodeAnalysis;
 using Xunit;
 
 namespace StripComments.Logic.Tests
@@ -5,7 +7,11 @@ namespace StripComments.Logic.Tests
     public class StripCommentsSolutionTest
     {
         [Fact(Skip = "Not implemented yet")]
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "xUnit1004:Test methods should not be skipped", Justification = "<Pending>")]
+        [SuppressMessage(
+            "Usage",
+            "xUnit1004:Test methods should not be skipped",
+            Justification = "<Pending>"
+        )]
         public void StripComments_CodeWarsTests()
         {
             Assert.Equal(
@@ -20,6 +26,43 @@ namespace StripComments.Logic.Tests
                 "a\nc\nd",
                 StripCommentsSolution.StripComments("a #b\nc\nd $e f g", new[] { "#", "$" })
             );
+        }
+
+        [Fact]
+        public void StripComments_NoComment_ReturnsSameString()
+        {
+            const string input = "anything";
+            Assert.Equal(input, StripCommentsSolution.StripComments(input, Array.Empty<string>()));
+        }
+
+        [Fact]
+        public void StripComments_SingleLineStartingWithComment_ReturnsBlank()
+        {
+            string[] commentSymbols = { "#" };
+            const string input = "# Hello World!";
+
+            Assert.Equal("", StripCommentsSolution.StripComments(input, commentSymbols));
+        }
+
+        [Fact]
+        public void StripComments_SingleLineStartingWithSecondComment_ReturnsBlank()
+        {
+            string[] commentSymbols = { "#", "//" };
+            const string input = "// Hello World!";
+
+            Assert.Equal("", StripCommentsSolution.StripComments(input, commentSymbols));
+        }
+
+        [Fact(Skip = "TODO: Implement this feature next.")]
+        [SuppressMessage("Usage", "xUnit1004:Test methods should not be skipped", Justification = "<Pending>")]
+        public void StripComments_TwoLinesWithCommentInSecondLine_ReturnsFirstLine()
+        {
+            string[] commentSymbols = { "#" };
+            const string firstLine = "Hello";
+            const string secondLine = "# World";
+            const string input = firstLine + "\n" + secondLine;
+
+            Assert.Equal(firstLine, StripCommentsSolution.StripComments(input, commentSymbols));
         }
     }
 }
--- StripComments/StripComments.Logic/StripCommentsSolution.cs
+++ StripComments/StripComments.Logic/StripCommentsSolution.cs
@@ -1,8 +1,24 @@
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+
 namespace StripComments.Logic
 {
     public static class StripCommentsSolution
     {
-        public static string StripComments(string text, string[] commentSymbols) =>
-            text + commentSymbols[0];
+        [SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "<Pending>")]
+        public static string StripComments(string text, string[] commentSymbols)
+        {
+            var result = text;
+
+            foreach (var commentSymbol in commentSymbols)
+            {
+                if (text.StartsWith(commentSymbol, StringComparison.CurrentCulture))
+                {
+                    result = "";
+                }
+            }
+            return result;
+        }
     }
 }
