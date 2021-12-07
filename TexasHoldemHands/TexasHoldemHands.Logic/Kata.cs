69a95221b05e21d2bcc00c6a12c213b0d3591366
Stefan Boos <sboos@igus.net>
2021-12-07 20:56:47 +0100

    refactor: simplify variable construction

--- TexasHoldemHands/TexasHoldemHands.Logic/Kata.cs
+++ TexasHoldemHands/TexasHoldemHands.Logic/Kata.cs
@@ -5,41 +5,51 @@ namespace TexasHoldemHands.Logic
 {
     public static class Kata
     {
-        public static (string type, string[] ranks) Hand(string[] holeCards, string[] communityCards)
+        private static readonly List<string> Ranks = new()
+        {
+            "A",
+            "K",
+            "Q",
+            "J",
+            "10",
+            "9",
+            "8",
+            "7",
+            "6",
+            "5",
+            "4",
+            "3",
+            "2"
+        };
+
+        public static (string type, string[] ranks) Hand(
+            string[] holeCards,
+            string[] communityCards
+        )
         {
             const string nothing = "nothing";
 
-            var allRanks = holeCards.Select(card => card[0..^1])
-                .Concat(communityCards.Select(card => card[0..^1]))
+            var allRanks = holeCards.Select(card => card[..^1])
+                .Concat(communityCards.Select(card => card[..^1]))
                 .ToList();
             allRanks.Sort(Descending);
 
-            var cardFrequencies = new Dictionary<string, int>()
-            {
-                { "A", 0 },
-                { "K", 0 },
-                { "Q", 0 },
-                { "J", 0 },
-                { "10", 0 },
-                { "9", 0 },
-                { "8", 0 },
-                { "7", 0 },
-                { "6", 0 },
-                { "5", 0 },
-                { "4", 0 },
-                { "3", 0 },
-                { "2", 0 },
-            };
+            var cardFrequencies = Ranks.ToDictionary(rank => rank, _ => 0);
             allRanks.ForEach(card => cardFrequencies[card]++);
             var rank = nothing;
             if (cardFrequencies.Any(bin => bin.Value == 2))
             {
-                var pair = "pair";
+                const string pair = "pair";
                 rank = pair;
             }
 
-            var pairRanks = cardFrequencies.Where(bin => bin.Value == 2).Select(bin => bin.Key).ToList();
-            var individualRanks = cardFrequencies.Where(bin => bin.Value == 1).Select(bin => bin.Key).Take(5 - 2 * pairRanks.Count).ToList();
+            var pairRanks = cardFrequencies.Where(bin => bin.Value == 2)
+                .Select(bin => bin.Key)
+                .ToList();
+            var individualRanks = cardFrequencies.Where(bin => bin.Value == 1)
+                .Select(bin => bin.Key)
+                .Take(5 - 2 * pairRanks.Count)
+                .ToList();
             var topRanks = pairRanks.Concat(individualRanks).ToArray();
 
             return (rank, topRanks);
@@ -47,10 +57,8 @@ namespace TexasHoldemHands.Logic
 
         private static int Descending(string x, string y)
         {
-            var ranks = new List<string> { "A", "K", "Q", "J", "10", "9", "8", "7", "6", "5", "4", "3", "2" };
-
-            var xIndex = ranks.IndexOf(x);
-            var yIndex = ranks.IndexOf(y);
+            var xIndex = Ranks.IndexOf(x);
+            var yIndex = Ranks.IndexOf(y);
 
             return xIndex < yIndex ? -1 : 1;
         }
