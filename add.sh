8d7743d928c0476570a8871fddd006b18e02afc4
Stefan Boos <kontakt@boos.systems>
2022-01-24 13:45:08 +0100

    style: fix static code analysis warnings in add.sh

--- add.sh
+++ add.sh
@@ -18,29 +18,29 @@
 #
 set -Eeufo pipefail
 
-read -p "Title: " TITLE
-read -p "Kyu: " KYU
-read -p "Source URL: " SOURCE
-read -p "Keywords (comma separated): " KEYWORDS
+read -rp "Title: " TITLE
+read -rp "Kyu: " KYU
+read -rp "Source URL: " SOURCE
+read -rp "Keywords (comma separated): " KEYWORDS
 
-DIR=$(echo "$TITLE" | sed -e 's/\s//g')
+DIR=${TITLE//\s/}
 LOGIC=${DIR}.Logic
 TEST=${LOGIC}.Tests
 
 echo Generating New Project
 echo ======================
 echo
-echo Title: $TITLE
-echo Kyu: $KYU
-echo Source: $SOURCE
-echo Keywords: $KEYWORDS
+echo "Title: $TITLE"
+echo "Kyu: $KYU"
+echo "Source: $SOURCE"
+echo "Keywords: $KEYWORDS"
 echo
-echo Directory: $DIR
-echo Logic: $DIR/$LOGIC/$LOGIC.csproj
-echo Tests: $DIR/$TEST/$TEST.csproj
+echo "Directory: $DIR"
+echo "Logic: $DIR/$LOGIC/$LOGIC.csproj"
+echo "Tests: $DIR/$TEST/$TEST.csproj"
 echo
 
-read -p "Press ENTER to continue" ENTER
+read -rp "Press ENTER to continue" ENTER
 
 mkdir "$DIR"
 pushd "$DIR"
@@ -66,4 +66,5 @@ popd
 dotnet sln katas-in-csharp.sln add "$DIR/$LOGIC/$LOGIC.csproj" --solution-folder "$DIR"
 dotnet sln katas-in-csharp.sln add "$DIR/$TEST/$TEST.csproj" --solution-folder "$DIR"
 
+echo "Adding coverage report to .netconfig ..."
 echo "    report = $DIR/$TEST/TestResults/*.xml" >> .netconfig
