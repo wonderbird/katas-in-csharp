eaca52ef8e35d84d2eaea279e3196a5404dbf458
Stefan Boos <kontakt@boos.systems>
2022-02-05 22:20:41 +0100

    test(snail): add test for 5x5 matrix

--- Snail/Snail.Logic.Tests/SnailSolutionTest.cs
+++ Snail/Snail.Logic.Tests/SnailSolutionTest.cs
@@ -24,17 +24,36 @@ namespace Snail.Logic.Tests
         }
 
         [Fact]
-        public void When3X3MatrixThenReturnSingleRound()
+        public void When3X3MatrixThenStopAtCenter()
         {
-            var input = new[] { new[] { 1, 2, 3 }, new[] { 4, 5, 6 }, new [] { 7, 8, 9} };
+            var input = new[] { new[] { 1, 2, 3 }, new[] { 4, 5, 6 }, new[] { 7, 8, 9 } };
             AssertEqualWithDetailedMessage(new[] { 1, 2, 3, 6, 9, 8, 7, 4, 5 }, SnailSolution.Snail(input));
         }
 
         [Fact]
-        public void When4X4MatrixThenReturnSingleRound()
+        public void When4X4MatrixThenReturnTwoRounds()
         {
-            var input = new[] { new[] { 1, 2, 3,4 }, new[] { 5, 6, 7, 8 }, new [] { 9, 10, 11, 12 }, new [] { 13, 14, 15, 16 } };
-            AssertEqualWithDetailedMessage(new[] { 1, 2, 3, 4, 8, 12, 16, 15, 14, 13, 9, 5, 6, 7, 11, 10 }, SnailSolution.Snail(input));
+            var input = new[]
+            {
+                new[] { 1, 2, 3, 4 }, new[] { 5, 6, 7, 8 }, new[] { 9, 10, 11, 12 }, new[] { 13, 14, 15, 16 }
+            };
+            AssertEqualWithDetailedMessage(new[] { 1, 2, 3, 4, 8, 12, 16, 15, 14, 13, 9, 5, 6, 7, 11, 10 },
+                SnailSolution.Snail(input));
+        }
+
+        [Fact]
+        public void When5X5MatrixThenReturnTwoAndAHalfRound()
+        {
+            var input = new[]
+            {
+                new[] { 1,  2,   3,  4,  5 },
+                new[] { 6,  7,   8,  9, 10 },
+                new[] { 11, 12, 13, 14, 15 },
+                new[] { 16, 17, 18, 19, 20 },
+                new[] { 21, 22, 23, 24, 25 }
+            };
+            AssertEqualWithDetailedMessage(new[] { 1, 2, 3, 4, 5, 10, 15, 20, 25, 24, 23, 22, 21, 16, 11, 6, 7, 8, 9, 14, 19, 18,17,12, 7, 8, 13 },
+                SnailSolution.Snail(input));
         }
 
         private static void AssertEqualWithDetailedMessage(int[] expected, int[] actual)
